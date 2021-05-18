24103e9562a944de31b4f0b2dccb7999dc2076b4
Roger Camargo <huogerac@gmail.com>
2021-05-18 13:12:01 -0300

    Atualiza formato da resposta do lista cartoes (empresa)

--- server/controllers/cartoesEmpresa.js
+++ server/controllers/cartoesEmpresa.js
@@ -1,10 +1,10 @@
-const data = require('../data');
+const data = require('../data')
 
 module.exports = {
   find: (req, res) => {
-    const { empresa_id } = req.params;
-    console.log(empresa_id);
-    if (empresa_id == 0) return [];
-    res.send({ data: data.cartoesEmpresa });
-  }
-};
+    const { empresa_id } = req.params
+    console.log(empresa_id)
+    if (empresa_id == 0) return []
+    res.send(data.cartoesEmpresa)
+  },
+}
