a57fa4465041bd293ac0b65f26d07d55dc1915a8
Roger Camargo <huogerac@gmail.com>
2021-08-10 17:29:23 -0300

    fix lint

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,30 +1,30 @@
-const data = require('../data');
+const data = require('../data')
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 function getTokenDoUsuario(phone, email, password) {
   if (phone != undefined) {
-    return data.tokens.find((token) => token.usuario.fone === phone);
+    return data.tokens.find((token) => token.usuario.fone === phone)
   } else {
     return data.tokens.find(
       (token) => token.usuario.email === email && password != undefined
-    );
+    )
   }
 }
 
 module.exports = {
   find: (req, res) => {
-    const { phone, email, password } = req.body;
-    let token = null;
+    const { phone, email, password } = req.body
+    let token = null
 
     sleep(800).then(() => {
-      token = getTokenDoUsuario(phone, email, password);
+      token = getTokenDoUsuario(phone, email, password)
       if (token == undefined) {
-        res.status(404).end();
+        res.status(404).end()
       }
-      res.send(token);
-    });
-  }
-};
+      res.send(token)
+    })
+  },
+}
