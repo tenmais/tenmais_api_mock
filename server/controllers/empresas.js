9e781621249630cef49404f5baed2a6c3675fc9a
Roger Camargo <huogerac@gmail.com>
2021-09-17 14:44:05 -0300

    Retorna 404 para empresa nao encontrada

--- server/controllers/empresas.js
+++ server/controllers/empresas.js
@@ -1,21 +1,29 @@
-const data = require('../data');
-const utils = require('../utils');
+const data = require('../data')
+const utils = require('../utils')
 
 function getEmpresaById(empresaId) {
-  return data.empresas.find((empresa) => empresa.id === empresaId);
+  return data.empresas.find((empresa) => empresa.id === empresaId)
 }
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 module.exports = {
   getEmpresa: (req, res) => {
-    const { empresa_id } = req.params;
-    const empresa = getEmpresaById(parseInt(empresa_id));
+    const { empresa_id } = req.params
+    const empresa = getEmpresaById(parseInt(empresa_id))
 
-    sleep(800).then(() => {
-      res.send(empresa);
-    });
-  }
-};
+    if (!empresa) {
+      res.status(404).send({
+        detail: `Empresa ID: '${empresa_id}' não encontrada`,
+        status: 404,
+        title: 'Not Found',
+      })
+    } else {
+      sleep(800).then(() => {
+        res.send(empresa)
+      })
+    }
+  },
+}
